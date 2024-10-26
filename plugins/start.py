f49a75af88fab831fe1f0df13774df222622f895
Unknown-903 <shubhamsinghal05072008@gmail.com>
2024-10-26 21:14:06 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,8 +92,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('⚡️ ᴍᴏᴠɪᴇs', url='https://t.me/+QVewP06XCPFiYWZl'),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇs', url='https://t.me/webseries_flix')
+                    InlineKeyboardButton('⚡️ Main', url='https://t.me/Anime_Bashes'),
+                    InlineKeyboardButton('🍁 Ongoing', url='https://t.me/Anime_New_Episodes')
                 ]
             ]
         )
